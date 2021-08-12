b47f411bdb41b56b1fb613abba30243b2dcc9416
SONGA-RENE <85831041+SONGA-RENE@users.noreply.github.com>
2021-08-12 10:25:47 +0200

    basics of our project

---
+++ Models/Store.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSRpharmacy.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200, ErrorMessage = "Name is required with less than 200 characters")]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(200, ErrorMessage = "Category is required with less than 200 characters")]
+        public string Category { get; set; }
+        [Required]
+        public float Price { get; set; }
+        [Required]
+        public string ImageName { get; set; }
+    }
+}
---
+++ Pages/Store.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSRpharmacy.Pages
+{
+    public class Store : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
